ccaedcc29e8ea3a6defa488ff24191eac62009dd
Hnam181 <nguyenhoainam1812000@gmail.com>
2023-12-19 11:09:05 +0700

    n

---
+++ main.js
@@ -0,0 +1,16 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
